9145f3f1a58ba7f57949f317d4f7b19c2e43fc49
Rendy Febry <rendyfebry@gmail.com>
2019-12-28 01:17:34 +0700

    Wiring

--- main.go
+++ main.go
@@ -1,19 +1,59 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
-	"os"
+	"io"
+	"io/ioutil"
+	"net/http"
 
-	"github.com/rendyfebry/go-graphql-example/internal/util/strings"
+	gqlRepo "github.com/rendyfebry/go-graphql-example/repository/graphql"
+
+	"github.com/graphql-go/graphql"
 )
 
+// GraphQLHandler ...
+func GraphQLHandler(w http.ResponseWriter, r *http.Request) {
+	buf, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer func() {
+		io.Copy(ioutil.Discard, r.Body)
+		r.Body.Close()
+	}()
+
+	var apolloQuery map[string]interface{}
+	if err := json.Unmarshal(buf, &apolloQuery); err != nil {
+		fmt.Println(err)
+		fmt.Println("Error on Unmarshalling!!!")
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
+
+	result := graphql.Do(graphql.Params{
+		Schema:        gqlRepo.UserSchema,
+		RequestString: apolloQuery["query"].(string),
+	})
+
+	if len(result.Errors) > 0 {
+		fmt.Println(fmt.Sprintf("wrong result, unexpected errors: %v", result.Errors))
+	}
+
+	resultByte, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resultByte)
+}
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter Yourname: ")
-
-	text, _ := reader.ReadString('\n')
-	sanitized := strings.SanitizeToAlphaNumeric(text)
-
-	fmt.Println(fmt.Sprintf("Hello, %s", sanitized))
+	http.HandleFunc("/graphql", GraphQLHandler)
+	fmt.Println("Now server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
