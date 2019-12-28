ab6cb8da53a14e4a997eff13433fcc86a8aad816
Rendy Febry <rendyfebry@gmail.com>
2019-12-28 23:00:25 +0700

    Change router to chi

--- main.go
+++ main.go
@@ -4,10 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/rendyfebry/go-graphql-example/controllers"
 )
 
@@ -17,11 +16,14 @@ func main() {
 
 	fmt.Println(fmt.Sprintf("Server listening on: %s", *port))
 
-	r := mux.NewRouter().StrictSlash(true)
-	r.Methods("POST").
-		Path("/graphql").
-		HandlerFunc(controllers.GraphQLHandler)
+	r := chi.NewRouter()
 
-	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
-	http.ListenAndServe(fmt.Sprintf(":%s", *port), loggedRouter)
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Post("/graphql", controllers.GraphQLHandler)
+	http.ListenAndServe(fmt.Sprintf(":%s", *port), r)
 }
